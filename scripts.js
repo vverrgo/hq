c8af49c4d5decbd1a066accd913d33b6a1f76126
vverrgo <alexemoryamaya@gmail.com>
2024-08-07 17:02:34 -0500

    made responsive nav bar

---
+++ scripts.js
@@ -0,0 +1,10 @@
+console.log('hey there')
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
